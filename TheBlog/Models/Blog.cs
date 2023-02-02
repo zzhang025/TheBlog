dae4b9547cf68ab179a802dbbef0db8b53123f90
zzhang025 <zhangzx45@gmail.com>
2023-02-01 19:56:52 -0800

    Data Model setup

---
+++ TheBlog/Models/Blog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace TheBlog.Models
+{
+	public class Blog
+	{
+		public int Id { get; set; }
+		public string AuthorId { get; set; }
+
+        [Required]
+		[StringLength(100,ErrorMessage ="The {0} must be at least {2} and at most {1}",MinimumLength =2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1}", MinimumLength = 2)]
+        public string Description { get; set; }
+
+		[DataType(DataType.Date)]
+		[Display(Name="Created Date")]
+		public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; }
+
+        [Display(Name = "Blog Image")]
+        public byte[]? ImageDate { get; set; }
+        [Display(Name = "Image Type")]
+        public string? ContentType { get; set; }
+
+		[NotMapped]
+		public IFormFile? Image { get; set; }
+
+        // Navigation Properties
+        public virtual IdentityUser Author { get; set; }
+        public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+
+
+    }
+}
+
---
+++ TheBlog/Models/Tag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace TheBlog.Models
+{
+	public class Tag
+	{
+		public int Id { get; set; }
+		public int PostId { get; set; }
+		public string AuthorId { get; set; }
+
+		[Required]
+        [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at most {1}", MinimumLength = 2)]
+		public string Text { get; set; }
+
+
+		// Navigation Properties
+		public virtual Post Post { get; set; }
+		public virtual IdentityUser Author { get; set; }
+	}
+}
+
